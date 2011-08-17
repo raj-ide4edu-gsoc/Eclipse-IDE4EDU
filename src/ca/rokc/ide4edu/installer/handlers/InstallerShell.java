2176c2bc8e9baa0649469e2a3aadd94bc7a8bf5c
Rajendra Kolli <rajendraatiitg@gmail.com>
2011-08-17 20:24:03 +0530

    Factoring the bunch of code into separate functions. Added comments for the procedures. Need to add documentation

--- src/ca/rokc/ide4edu/installer/handlers/InstallerShell.java
+++ src/ca/rokc/ide4edu/installer/handlers/InstallerShell.java
@@ -42,180 +42,42 @@ import ca.rokc.ide4edu.installer.InstallerFeature;
 
 public class InstallerShell {
 
-	private Shell shell;
-	private IProvisioningAgent agent;
-	IProgressMonitor monitor;
-
-	public InstallerShell(Shell parent) {
-		
-		List<InstallerFeature> addons_list = new ArrayList<InstallerFeature>();
-
-			Activator.populateData(addons_list);
-		shell = new Shell(parent);
-		shell.setLayout(new GridLayout(3, true));		
-		shell.setText("Install New Language");
-		shell.setBounds(100, 100, 600, 400);
-		GridData gridData = new GridData(GridData.FILL_BOTH);
-		gridData.widthHint = 200;
-		gridData.heightHint = 100;
-		gridData.grabExcessHorizontalSpace = false;
-		gridData.grabExcessVerticalSpace = false;
-		for (final InstallerFeature feature : addons_list) {
-			Button selectionButton = new Button(shell, SWT.PUSH);
-			selectionButton.setLayoutData(gridData);
+		private Shell shell;
+		private IProvisioningAgent agent;
+		IProgressMonitor monitor;
+	
+		public InstallerShell(Shell parent) {
 			
-			final Image image = getImageForFeature(shell.getDisplay(), feature);
-			selectionButton.setImage(image);
-			selectionButton.addDisposeListener(new DisposeListener() {				
-				@Override
-				public void widgetDisposed(DisposeEvent e) {
-					image.dispose();
-				}
-			});
-			
-			selectionButton.setText("Install " + feature.getTitleName());
-			selectionButton.addSelectionListener(new SelectionAdapter() {
-				
-				
-
-				@Override
-				public void widgetSelected(SelectionEvent e) {
-					
-
-					try {
-						/*
-						 * This test Shell is intended to display the description of the packages
-						 * and prompt the user to install or close the window.
-						 * If handled properly, this can be the way a package can be neatly described to the user
-						 * 
-						 * */
-						Display display;
-						display = PlatformUI.getWorkbench().getDisplay();
-						final Shell test = new Shell(display);
-						
-						test.setText("About the package - "+feature.getTitleName());
-						test.setBounds(100, 100, 600, 400);
-						test.setLayout(null);
-						/*
-						 * Labels are defined to show the image and the description
-						 * */
-						Label label = new Label();
-						label.setBounds(200, 200, 380, 180);
-						label.setText(feature.getDescriptionContent());
-						final Button installButton = new Button(test, SWT.PUSH);
-						installButton.setBounds(380, 330, 100, 60);
-						installButton.setText("Install");
-						final Button closeButton = new Button(test, SWT.PUSH);
-						closeButton.setBounds(490, 330, 100, 60);
-						closeButton.setText("Close");
-						shell.setVisible(false);
-						test.open();
-						/*
-						 * Implementation of methods for the buttons and the installation procedure.
-						 * Close Button calls the shell and widget disposal functions.
-						 * Install button calls the installation procedure to be executed
-						 * */
-						closeButton.addSelectionListener(new SelectionAdapter(){
-							public void widgetSelected(SelectionEvent e){
-								test.dispose();
-							}
-						});
-						/*
-						 * Installation Job is being handled here. It is supposed to work but as of now
-						 * It only queries data from a given metadata repository.
-						 * The issue is being that the installation job is being created but never triggered
-						 * */
-						installButton.addSelectionListener(new SelectionAdapter(){
-							@SuppressWarnings("unchecked")
-							public void widgetSelected(SelectionEvent e){
-								// get the agent
-								@SuppressWarnings("rawtypes")
-								ServiceReference sr = Activator.getContext().getServiceReference(
-										IProvisioningAgentProvider.SERVICE_NAME);
-								IProvisioningAgentProvider agentProvider = null;
-								
-								agentProvider = (IProvisioningAgentProvider) Activator.getContext()
-										.getService(sr);
-								try {
-									agent = agentProvider.createAgent(null);
-								} catch (ProvisionException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								}
-								// get the repository managers and define our repositories
-								IMetadataRepositoryManager manager = (IMetadataRepositoryManager) agent
-										.getService(IMetadataRepositoryManager.SERVICE_NAME);
-								IArtifactRepositoryManager artifactManager = (IArtifactRepositoryManager) agent
-										.getService(IArtifactRepositoryManager.SERVICE_NAME);
-								try {
-									manager.addRepository(new URI(
-											"http://download.eclipse.org/releases/indigo"));
-								} catch (URISyntaxException e1) { // TODO Auto-generated catch block
-									e1.printStackTrace();
-								}
-								try {
-									artifactManager.addRepository(new URI(
-											"http://download.eclipse.org/releases/indigo"));
-								} catch (URISyntaxException e1) { // TODO Auto-generated catch block
-									e1.printStackTrace();
-								}
-								// Load and query the metadata
-								IMetadataRepository metadataRepo = null;
-								try {
-									System.out.println("The execution of the installation has beguin");
-									metadataRepo = manager.loadRepository(new URI(
-											"http://download.eclipse.org/releases/indigo"),
-											new NullProgressMonitor());
-								} catch (ProvisionException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								} catch (OperationCanceledException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								} catch (URISyntaxException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								}
-								/*
-								 * Create Variable Queries based on the package name
-								 * To ensure the correctness of the query, the package name used
-								 * should be as accurate as the one used in actual terms
-								 * */
-								@SuppressWarnings("unused")
-								Collection<IInstallableUnit> toInstall = metadataRepo.query(
-										QueryUtil.createIUQuery("org.eclipse."+feature.getPackageName()+".feature.group"),
-										new NullProgressMonitor()).toUnmodifiableSet();
-								
-								//TODO : The installation procedure must be written from here to complete the plugin
-
-							}
-						});
-						
-						test.addDisposeListener(new DisposeListener(){
-
-							@Override
-							public void widgetDisposed(DisposeEvent e) {
-								installButton.dispose();
-								closeButton.dispose();
-								shell.update();
-								shell.setVisible(true);
-								
-							}
-							
-							
-						});
-
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-
-				}
-			});
-			selectionButton.pack();
-
+			List<InstallerFeature> addons_list = new ArrayList<InstallerFeature>();
+	
+				Activator.populateData(addons_list);
+			shell = new Shell(parent);
+			shell.setLayout(new GridLayout(3, true));		
+			shell.setText("Install New Language");
+			shell.setBounds(100, 100, 600, 400);
+			GridData gridData = new GridData(GridData.FILL_BOTH);
+			gridData.widthHint = 200;
+			gridData.heightHint = 100;
+			gridData.grabExcessHorizontalSpace = false;
+			gridData.grabExcessVerticalSpace = false;
+			for (final InstallerFeature feature : addons_list) {
+				Button selectionButton = new Button(shell, SWT.PUSH);
+				selectionButton.setLayoutData(gridData);
+				setButtonDetails(shell, selectionButton, feature);	//The package deatils and functionalities are set in a separate procedure call "setButtonDetails"
+				selectionButton.pack();
+	
+			}
+			shell.pack();
 		}
-		shell.pack();
-	}
+		
+		
+	
+	
+	
+	/*
+	 * This function loads the images obtained from URL 
+	 * By opening the stream and returning an image
+	 * */
 	private Image getImageForFeature(Device device, InstallerFeature feature) {
 		InputStream in = null;
 		try {
@@ -235,6 +97,182 @@ public class InstallerShell {
 					e.printStackTrace();
 				}
 		}
+	}
+	
+	
+	/*
+	 * This procedure can be used to set the details of the button we
+	 * use for installing the packages
+	 * */
+	private void setButtonDetails(final Shell shell, Button selectionButton, final InstallerFeature feature){
+		final Image image = getImageForFeature(shell.getDisplay(), feature);
+		selectionButton.setImage(image);
+		selectionButton.addDisposeListener(new DisposeListener() {				
+			@Override
+			public void widgetDisposed(DisposeEvent e) {
+				image.dispose();
+			}
+		});
+		
+		selectionButton.setText("Install " + feature.getTitleName());
+		selectionButton.addSelectionListener(new SelectionAdapter() {
+			
+			
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+
+				try {
+					/*
+					 * This packageDetailer Shell is intended to display the description of the packages
+					 * and prompt the user to install or close the window.
+					 * If handled properly, this can be the way a package can be neatly described to the user
+					 * 
+					 * */
+					Display display;
+					display = PlatformUI.getWorkbench().getDisplay();
+					final Shell packageDetailer = new Shell(display);
+					
+					packageDetailer.setText("About the package - "+feature.getTitleName());
+					packageDetailer.setBounds(100, 100, 600, 400);
+					packageDetailer.setLayout(null);
+					/*
+					 * Labels are defined to show the image and the description
+					 * */
+					Label label = new Label();
+					label.setBounds(200, 200, 380, 180);
+					label.setText(feature.getDescriptionContent());
+					final Button installButton = new Button(packageDetailer, SWT.PUSH);
+					installButton.setBounds(380, 330, 100, 60);
+					installButton.setText("Install");
+					final Button closeButton = new Button(packageDetailer, SWT.PUSH);
+					closeButton.setBounds(490, 330, 100, 60);
+					closeButton.setText("Close");
+					shell.setVisible(false);
+					packageDetailer.open();
+					/*
+					 * Implementation of methods for the buttons and the installation procedure.
+					 * Close Button calls the shell and widget disposal functions.
+					 * Install button calls the installation procedure to be executed
+					 * */
+					closeButton.addSelectionListener(new SelectionAdapter(){
+						public void widgetSelected(SelectionEvent e){
+							packageDetailer.dispose();
+						}
+					});
+					/*
+					 * Installation Job is being handled here. It is supposed to work but as of now
+					 * It only queries data from a given metadata repository.
+					 * The issue is being that the installation job is being created but never triggered
+					 * */
+					installButton.addSelectionListener(new SelectionAdapter(){
+
+						public void widgetSelected(SelectionEvent e){
+							
+							installationProcedure(feature.getPackageName());
+						}
+					});
+					
+					packageDetailer.addDisposeListener(new DisposeListener(){
+
+						@Override
+						public void widgetDisposed(DisposeEvent e) {
+							installButton.dispose();
+							closeButton.dispose();
+							shell.update();
+							shell.setVisible(true);
+							
+						}
+						
+						
+					});
+
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+
+			}
+		});		
+	}
+	
+	/*
+	 * This procedure when invoked calls the Installation procedure for a particular package
+	 * It basically creates an agent, makes a metadata repository and queries for a particular
+	 * Feature package. When obtained, it tries to install the package into the IDE, provided
+	 * There are no extra conflicts like the package number being more than one. If some conflict 
+	 * occurs 
+	 * */
+	@SuppressWarnings("unchecked")
+	private void installationProcedure(String packageId){
+		
+		@SuppressWarnings("rawtypes")
+		
+		ServiceReference sr = Activator.getContext().getServiceReference(IProvisioningAgentProvider.SERVICE_NAME);
+		IProvisioningAgentProvider agentProvider = null;
+		
+		agentProvider = (IProvisioningAgentProvider) Activator.getContext().getService(sr);
+		try {
+				agent = agentProvider.createAgent(null);
+		} catch (ProvisionException e1) {
+			// TODO Auto-generated catch block
+				e1.printStackTrace();
+		}
+		
+		// get the repository managers and define our repositories
+		IMetadataRepositoryManager manager = (IMetadataRepositoryManager) agent
+				.getService(IMetadataRepositoryManager.SERVICE_NAME);
+		IArtifactRepositoryManager artifactManager = (IArtifactRepositoryManager) agent
+				.getService(IArtifactRepositoryManager.SERVICE_NAME);
+		
+		try {
+				manager.addRepository(new URI("http://download.eclipse.org/releases/indigo"));
+		
+		} catch (URISyntaxException e1) { 
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+		}
+		try {
+				artifactManager.addRepository(new URI("http://download.eclipse.org/releases/indigo"));
+		} catch (URISyntaxException e1) { 
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+		}
+		
+		/* 
+		 * A metadata repository existing at a remote server is located and
+		 * loaded onto a variable on the user side. This enables us to query for 
+		 * the particular package and install it into IDE
+		 * 
+		 * */
+		IMetadataRepository metadataRepo = null;
+		try {
+				metadataRepo = manager.loadRepository(new URI("http://download.eclipse.org/releases/indigo"),new NullProgressMonitor());
+		} catch (ProvisionException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+		} catch (OperationCanceledException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+		} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+		}
+		
+		
+		/*
+		 * Create Variable Queries based on the package name
+		 * To ensure the correctness of the query, the package name used
+		 * should be as accurate as the one used in actual terms
+		 * */
+		@SuppressWarnings("unused")
+		Collection<IInstallableUnit> toInstall = metadataRepo.query(
+					QueryUtil.createIUQuery("org.eclipse."+packageId+".feature.group"),
+					new NullProgressMonitor()).toUnmodifiableSet();
+		
+		//TODO : The installation procedure must be written from here to complete the plugin
+		
+		
 	}
 	
 	public void open() {
